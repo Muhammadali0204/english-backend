1be6e53000ae9c07f083abcdb04b3cd8fdcf5a00
muhammadali0204 <mamadjonovmuhammadali7@gmail.com>
2025-05-31 12:27:05 +0500

    add root path

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.routers.main import router
 from app.core.config import DATABASE_CONFIG
 
 
-app = FastAPI(openapi_version="3.1.0", redirect_slashes=True)
+app = FastAPI(openapi_version="3.1.0", redirect_slashes=True, root_path='/api')
 
 
 app.add_middleware(
@@ -20,4 +20,4 @@ app.add_middleware(
 
 register_tortoise(app, config=DATABASE_CONFIG, generate_schemas=True)
 
-app.include_router(router, prefix='/api')
+app.include_router(router)
